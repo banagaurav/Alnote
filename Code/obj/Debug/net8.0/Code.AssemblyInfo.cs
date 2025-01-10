8af03b877301527e01cc75502d7cfbfb8f30d3e7
Your Name <gauravbana840@gmail.com>
2025-01-10 20:25:40 +0545

    signin complete

--- Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
+++ Code/obj/Debug/net8.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044f51e43488b29a1269468316ce68fc5a6bddb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3aa9143e23525733a076121b78cceb73ce2e886")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
