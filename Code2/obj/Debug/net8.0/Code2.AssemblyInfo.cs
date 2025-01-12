2926e1b213a0f5efe2c8738a16a40218af39b19b
Your Name <gauravbana840@gmail.com>
2025-01-12 16:07:57 +0545

    add pdfDto

--- Code2/obj/Debug/net8.0/Code2.AssemblyInfo.cs
+++ Code2/obj/Debug/net8.0/Code2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238d20bb19922fc1412977d7d48b1c54caf1a4df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05fb6153eb99e1b2423a00d027fcc2f970b2955")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
